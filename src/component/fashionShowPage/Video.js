2b96ee87822dbf780bdf638c1e21ea6e1eeff481
SusannaShu <82421710+SusannaShu@users.noreply.github.com>
2021-12-30 03:01:17 +0800

    ok

--- src/component/fashionShowPage/Video.js
+++ src/component/fashionShowPage/Video.js
@@ -3,7 +3,7 @@ import { Embed } from 'semantic-ui-react'
 
 const Video = () => (
   <Embed
-    icon='right circle arrow'
+  icon='right circle play'
     placeholder='https://i.loli.net/2020/11/14/4OnD3pqtlIwaUiV.png'
     url='http://23927233.s21v.faiusr.com/58/ABUIABA6GAAg1NC0_AUolayKzwE.mp4'
   />
