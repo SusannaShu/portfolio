b95585f91e39ef7ecbebe0d98b8cfe6dd919fcb7
SusannaShu <susanna.xinshu@gmail.com>
2024-09-10 19:31:25 -0400

    added tags for portfolios

--- src/component/worksPage/WorksPage.js
+++ src/component/worksPage/WorksPage.js
@@ -5,51 +5,130 @@ import './workPage.css'
 import axios from 'axios'
 import Footer from '../footer/Foot.js'
 import Loading from '../loader/Loading.js'
+import { Label, Icon } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
 
 class WorksPage extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
       items: [],
-      loading: true
+      workTags: [],
+      loading: true,
+      selectedTags: []
     }
-
   }
 
-  componentDidMount () {
+  componentDidMount() {
     window.scrollTo(0, 0)
-    const self=this
-    axios(
-      'https://sheyou-backend.herokuapp.com/portfolios?id_gt=1&_sort=id:asc',
-      {
-        method: 'get'
-      }
-    ).then(function (res) {
-      if (res.status === 200) {
-        console.log(res.data)
-        const displayItems = res.data.filter(item => item.display !== undefined && item.display !== null && item.display !== '');
-        const nonDisplayItems = res.data.filter(item => item.display === undefined || item.display === null || item.display === '');
-        const sortedItems = [...displayItems, ...nonDisplayItems];
-        self.setState({ items: sortedItems, loading: false });
-      } else {
-        alert('something went wrong :(')
-        self.setState({ loading: false }); // Also update loading state in case of error
-      }
-    })
+    this.fetchData()
   }
 
-  render () {
-    const { items, loading } = this.state;
-    console.log(items)
-    if (loading) {
-      return <Loading/>
+  fetchData = async () => {
+    try {
+      const [portfoliosResponse, workTagsResponse] = await Promise.all([
+        axios.get(
+          'https://sheyou-backend.herokuapp.com/portfolios?id_gt=1&_sort=id:asc'
+        ),
+        axios.get('https://sheyou-backend.herokuapp.com/work-tags')
+      ])
+
+      if (
+        portfoliosResponse.status === 200 &&
+        workTagsResponse.status === 200
+      ) {
+        const displayItems = portfoliosResponse.data.filter(
+          item =>
+            item.display !== undefined &&
+            item.display !== null &&
+            item.display !== ''
+        )
+        const nonDisplayItems = portfoliosResponse.data.filter(
+          item =>
+            item.display === undefined ||
+            item.display === null ||
+            item.display === ''
+        )
+        const sortedItems = [...displayItems, ...nonDisplayItems]
+
+        this.setState({
+          items: sortedItems,
+          workTags: workTagsResponse.data,
+          loading: false
+        })
+      } else {
+        throw new Error('Failed to fetch data')
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error)
+      alert('Something went wrong :(')
+      this.setState({ loading: false })
     }
+  }
+
+  handleTagClick = async (tagName) => {
+    const { selectedTags } = this.state;
+    let newSelectedTags;
+
+    if (selectedTags.includes(tagName)) {
+      newSelectedTags = selectedTags.filter(tag => tag !== tagName);
+    } else {
+      newSelectedTags = [...selectedTags, tagName];
+    }
+
+    this.setState({ selectedTags: newSelectedTags, loading: true });
+
+    try {
+      const queryString = newSelectedTags.map(tag => `tagName=${tag}`).join('&');
+      const response = await axios.get(`https://sheyou-backend.herokuapp.com/work-tags?${queryString}`);
+      
+      if (response.status === 200) {
+        const filteredItems = response.data.flatMap(tag => tag.portfolios);
+        this.setState({ items: filteredItems, loading: false });
+      } else {
+        throw new Error('Failed to fetch filtered data');
+      }
+    } catch (error) {
+      console.error('Error fetching filtered data:', error);
+      alert('Something went wrong while filtering :(');
+      this.setState({ loading: false });
+    }
+  }
+
+  render() {
+    const { items, workTags, loading, selectedTags } = this.state;
+    
+    if (loading) {
+      return <Loading />
+    }
+    
     return (
       <div>
         <Nav />
+        
+        <div className='work-tags' style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          paddingTop: '60px',
+          gap: '8px',
+          backgroundColor: 'pink',
+          justifyContent: 'center'
+        }}>
+          {workTags.map((tag) => (
+            <Label 
+              key={tag.id} 
+              as='a' 
+              color={selectedTags.includes(tag.tagName) ? 'pink' : null}
+              onClick={() => this.handleTagClick(tag.tagName)}
+            >
+              {selectedTags.includes(tag.tagName) && <Icon name='close' />}
+              {tag.tagName}
+            </Label>
+          ))}
+        </div>
 
         <div className='item-group'>
-          {this.state.items.map((item, index) => (
+          {items.map((item, index) => (
             <CardItem key={index} item={item} />
           ))}
         </div>
